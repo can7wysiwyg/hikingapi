76c85722fbc1951597274617721cc5d840523090
Пол Касуса <65332516+can7wysiwyg@users.noreply.github.com>
2025-01-04 10:59:58 +0200

    yays

--- models/TaxiRouteModel.js
+++ models/TaxiRouteModel.js
@@ -17,3 +17,5 @@ const RouteSchema = new mongoose.Schema({
   fare: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
 });
+
+module.exports = mongoose.model('TaxiRoute', RouteSchema)
