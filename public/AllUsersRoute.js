2d4d10325fd97d40dc7322835f6f4baea5cc3364
Пол Касуса <65332516+can7wysiwyg@users.noreply.github.com>
2025-01-08 09:25:15 +0200

    concrete jungle

--- public/AllUsersRoute.js
+++ public/AllUsersRoute.js
@@ -14,6 +14,29 @@ AllUsersRoute.get('/show_all_users', verify,  asyncHandler(async (req, res) => {
       res.status(500).json({ msg: `There was an error in getting the users: ${error.message}` });
     }
   }));
+
+
+  AllUsersRoute.get('/show_user/:id', verify, asyncHandler(async(req, res) => {
+
+try {
+
+  const {id} = req.params
+
+  const user = await User.findOne({_id: id}).select('fullname _id userLocation');
+
+
+  res.json({user})
+
+  
+
+  
+} catch (error) {
+
+  res.status(500).json({ msg: `There was an error in getting the users: ${error.message}` });
+  
+}
+
+  }) )
   
 
 
