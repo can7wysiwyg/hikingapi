cf2e9d3be8cb8c0ed72f5258c2ed9d152b981e19
Пол Касуса <65332516+can7wysiwyg@users.noreply.github.com>
2025-01-31 09:44:50 +0200

    sdnf

--- adminRoutes/AdminUsersRoute.js
+++ adminRoutes/AdminUsersRoute.js
@@ -4,6 +4,8 @@ const Driver = require("../models/DriverModel");
 const verifyMainAdmin = require("../adminmiddleware/verifyMainAdmin");
 const mainAdmin = require("../adminmiddleware/mainAdmin");
 const asyncHandler = require("express-async-handler");
+const nodemailer = require('nodemailer')
+
 
 AdminUsersRoute.get(
   "/admin_show_all_users",
@@ -67,15 +69,79 @@ AdminUsersRoute.get(
 
     const driver = await Driver.findOne({ driverName: id });
 
-
     if (!driver) {
       return res.json({ msg: "USER HAS NO DRIVER APPLICATION" });
     }
 
-
-  
     res.json(driver);
   })
 );
+
+AdminUsersRoute.put(
+  "/admin_approve_driver_application/:id",
+  verifyMainAdmin,
+  mainAdmin,
+  asyncHandler(async (req, res) => {
+    try {
+      const { id } = req.params;
+      const findApp = await Driver.findOne({ driverName: id });
+
+      let appId = findApp._id;
+
+      await Driver.findByIdAndUpdate(appId, req.body, { new: true });
+
+      res.json({ msg: "Success!" });
+    } catch (error) {
+      res.json({ msg: `try again later` });
+    }
+  })
+);
+
+
+AdminUsersRoute.delete('/admin_delete_application/:id', verifyMainAdmin, mainAdmin, asyncHandler(async(req, res) => {
+
+  try {
+    const {id} = req.params
+
+    const user = await User.findById(id)
+    const email = user.email
+
+    const findApp = await Driver.findOne({driverName: id})
+
+    const userId =  findApp._id
+
+
+    await Driver.findByIdAndDelete(userId)
+
+
+    const transporter = nodemailer.createTransport({
+          service: 'gmail', 
+          auth: { user: process.env.EMAIL_USER, 
+                 pass: process.env.EMAIL_PASSWORD  
+          }
+      });
+     
+    
+        // Send verification code to user's email
+        const mailOptions = {
+          from: process.env.EMAIL_USER,
+          to: email,
+          subject: 'Driver Application ',
+          text: `Your application as driver for Kalichanngu transport services has been rejected`,
+        };
+    
+        await transporter.sendMail(mailOptions);
+
+
+        res.json({msg: "user will be emailed"})
+    
+
+    
+  } catch (error) {
+    res.json({msg: `try again later`})
+  }
+
+
+}))
 
 module.exports = AdminUsersRoute;
