4ae46e56e4a7989cb3c10f8ec6b52f50af085568
Пол Касуса <65332516+can7wysiwyg@users.noreply.github.com>
2025-03-03 09:52:13 +0200

    hmm

--- driverRoutes/DriverTaxiRoute.js
+++ driverRoutes/DriverTaxiRoute.js
@@ -118,7 +118,7 @@ DriverTaxiRoute.post('/driver/routes', verify, verifyDriver, async (req, res) =>
 
         const driver = await Driver.findOne({driverName: id})
 
-        const myRoutes = await TaxiRoute.findOne({taxiId: driver._id})
+        const myRoutes = await TaxiRoute.find({taxiId: driver._id})
 
         res.json(myRoutes)
         
