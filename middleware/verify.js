8fb491d7ab3d46d6a60a2450f5494f038eb85a0d
Пол Касуса <65332516+can7wysiwyg@users.noreply.github.com>
2025-03-18 10:36:11 +0200

    cool

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,86 +1,39 @@
-// const jwt = require('jsonwebtoken')
-// const asyncHandler = require('express-async-handler')
-// const User = require('../models/UserModel')
-
-// const verify = asyncHandler(async (req, res, next) => {
-//   let usertoken
-
-//   if (
-//     req.headers.authorization &&
-//     req.headers.authorization.startsWith('Bearer')
-//   ) {
-//     try {
-//       // Get token from header
-//       usertoken = req.headers.authorization.split(' ')[1]
-
-//       // Verify token
-//       const decoded = jwt.verify(usertoken, process.env.ACCESS_TOKEN)
-
-//       // Get user from the token
-//       req.user = await User.findById(decoded.id).select('-password')
-
-//       next()
-//     } catch (error) {
-//       console.log(error)
-//       res.status(401)
-//       throw new Error('Not authorized')
-//     }
-//   }
-
-//   if (!usertoken) {
-//     res.status(401)
-//     throw new Error('Not authorized, no token')
-//   }
-// })
-
-// module.exports = verify 
-
-
-
-const jwt = require('jsonwebtoken');
-const asyncHandler = require('express-async-handler');
-const User = require('../models/UserModel');
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/UserModel')
 
 const verify = asyncHandler(async (req, res, next) => {
-  let usertoken;
-  
+  let usertoken
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     try {
       // Get token from header
-      usertoken = req.headers.authorization.split(' ')[1];
-      
-      // Verify token
-      const decoded = jwt.verify(usertoken, process.env.ACCESS_TOKEN);
-      
-      // Get user from the token
-      req.user = await User.findById(decoded.id).select('-password');
-      
-      if (!req.user) {
-        res.status(401);
-        throw new Error('User not found');
-      }
-      
-      next();
-    } catch (error) {
-      console.log(error);
-      
-      // Provide more specific error message for token expiration
-      if (error.name === 'TokenExpiredError') {
-        res.status(401);
-        throw new Error('Token expired, please log in again');
-      } else {
-        res.status(401);
-        throw new Error('Not authorized');
-      }
-    }
-  } else if (!usertoken) {
-    res.status(401);
-    throw new Error('Not authorized, no token');
-  }
-});
+      usertoken = req.headers.authorization.split(' ')[1]
 
-module.exports = verify;
+      // Verify token
+      const decoded = jwt.verify(usertoken, process.env.ACCESS_TOKEN)
+
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select('-password')
+
+      next()
+    } catch (error) {
+      console.log(error)
+      res.status(401)
+      console.log('Not authorized')
+    }
+  }
+
+  if (!usertoken) {
+    
+    console.log('Not authorized, no token')
+  }
+})
+
+module.exports = verify 
+
+
 
